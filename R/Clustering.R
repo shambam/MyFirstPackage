0b9741a2cdc2760cbbcbd62733d827acaa7ac17d
shambam <shamit.soneji@med.lu.se>
2019-02-08 13:44:08 +0100

    name change

--- R/Clustering.R
+++ R/Clustering.R
@@ -1,8 +1,8 @@
 #'Clusters the data and splits the dendrogram into k clusters
 #'@param sco An scell S4 object
 #'@param k The number of clusters we wish to retrieve
-#'@export clusterData
-clusterData <- function(sco,k){
+#'@export clusterCells
+clusterCells <- function(sco,k){
   
   hc <- hclust(as.dist(1-cor(t(sco$data))))
   sco@clusters <- cutree(hc,k)
