906cadd36bd0f13edb84fad1dbf892c9995a4522
shambam <shamit.soneji@med.lu.se>
2019-02-11 09:16:39 +0100

    colour tsne by cluster

--- R/Graphs.R
+++ R/Graphs.R
@@ -14,13 +14,25 @@ MakeHeatMap <- function(sco){
 #'
 ViolinPlotFACSfromCluster <- function(sco,clusID){
   
-  req.cell <- which(sco@cell.clusters==clusID)
+  req.cell <- which(sco@cell.clusters==clusID) #find which cells belong to the cluster we need
   
-  req.facs <- sco@facs[,req.cell]
-  req.facs  <- as.data.frame(t(req.facs))
+  req.facs <- sco@facs[,req.cell] #extract these cells from the facs data
+  req.facs  <- as.data.frame(t(req.facs)) ## vioplot needs a data.frame to make vioplot per surface marker
 
   #vio.data <- as.list(as.data.frame(t(req.facs)))
-  vioplot(req.facs,names=colnames(req.facs),main=paste("FACS data from cluster ",clusID))
+  vioplot(req.facs,names=colnames(req.facs),main=paste("FACS data from cluster ",clusID)) #make a viloin plot of this data
   #print(names(vio.data))
 
+}
+
+#'Makes a 3D tSNE plot where cells are coloured by cluster
+#'@param sco
+#'@export ColourTSNEbyClust
+
+ColourTSNEbyClust <- function(sco){
+
+  #We need to make a colour plalette of divergent colours using Rcolorbrewer. This lib has been added to the dependencies.
+
+  colrs <- brewer.pal(max(hspc@cell.clusters), "Set1") #make as many colours as we have clusters Set1 is a range of divergent colours
+  rgl.points(sco@tsne,col=colrs[sco@cell.clusters]) # the last part makes a vector of the colours from the cluster information.
 }
